6a1ff4da6bf85eb6aa922d49cbad0c1ef91abd79
Pablo Villota <pvillota@cuenca.gob.ec>
2022-06-19 14:05:40 -0500

    botón aceptar

--- front_end_carnales/src/pages/pos.js
+++ front_end_carnales/src/pages/pos.js
@@ -3,91 +3,91 @@ import Header from '../components/headerGen';
 import "../stylesheets/pos.css";
 
 let mesas = [
-  {nombre: "Ventana"},
-  {nombre: "Mesa 1"},
-  {nombre: "Mesa 2"},
-  {nombre: "Mesa 3"},
-  {nombre: "Mesa 4"},
-  {nombre: "Mesa 5"},
-  {nombre: "Mesa 6"},
-  {nombre: "Mesa 7"}
+  {name: "Ventana"},
+  {name: "Mesa 1"},
+  {name: "Mesa 2"},
+  {name: "Mesa 3"},
+  {name: "Mesa 4"},
+  {name: "Mesa 5"},
+  {name: "Mesa 6"},
+  {name: "Mesa 7"}
 ];
 
 let clientes = [
-  {nombre: "General"},
-  {nombre: "Cliente 1"},
-  {nombre: "Cliente 2"},
-  {nombre: "Cliente 3"},
-  {nombre: "Cliente 4"},
-  {nombre: "Cliente 5"},
-  {nombre: "Cliente 6"},
-  {nombre: "Cliente 7"},
-  {nombre: "Cliente 8"},
+  {name: "General"},
+  {name: "Cliente 1"},
+  {name: "Cliente 2"},
+  {name: "Cliente 3"},
+  {name: "Cliente 4"},
+  {name: "Cliente 5"},
+  {name: "Cliente 6"},
+  {name: "Cliente 7"},
+  {name: "Cliente 8"},
 ];
 
 let categorias = [
-  {_id: 1, nombre: "Taco"},
-  {_id: 2, nombre: "Taco Chingón"},
-  {_id: 3, nombre: "Burrito"},
-  {_id: 4, nombre: "Quesadilla"},
-  {_id: 5, nombre: "Especiales"},
-  {_id: 6, nombre: "Bebidas"}
+  {_id: 1, name: "Taco"},
+  {_id: 2, name: "Taco Chingón"},
+  {_id: 3, name: "Burrito"},
+  {_id: 4, name: "Quesadilla"},
+  {_id: 5, name: "Especiales"},
+  {_id: 6, name: "Bebidas"}
 ];
 
 let productos = [
-  {nombre:'Taco Pollo Asado', alias: "Pollo Asado", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Carne Asada', alias: "Carne Asada", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Carne Guisada', alias: "Carne Guisada", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Al Pastor', alias: "Al Pastor", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla','Piña']},
-  {nombre:'Taco Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Chicharron', alias: "Chicharron", idCategoria: 1, precio: 1.5, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Carnitas', alias: "Carnitas", idCategoria: 1, precio: 1.5, eliminables:['Aguacate','Cebolla']},
-  {nombre:'Taco Cabeza', alias: "Cabeza", idCategoria: 1, precio: 1.5, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Pollo Asado', alias: "Pollo Asado", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Carne Asada', alias: "Carne Asada", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Carne Guisada', alias: "Carne Guisada", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Al Pastor', alias: "Al Pastor", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla','Piña']},
+  {name:'Taco Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 1, precio: 1, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Chicharron', alias: "Chicharron", idCategoria: 1, precio: 1.5, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Carnitas', alias: "Carnitas", idCategoria: 1, precio: 1.5, eliminables:['Aguacate','Cebolla']},
+  {name:'Taco Cabeza', alias: "Cabeza", idCategoria: 1, precio: 1.5, eliminables:['Aguacate','Cebolla']},
   
-  {nombre:'Taco Chingón Pollo Asado', alias: "Pollo Asado", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Carne Asada', alias: "Carne Asada", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Carne Guisada', alias: "Carne Guisada", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Al Pastor', alias: "Al Pastor", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla', 'Piña']},
-  {nombre:'Taco Chingón Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Chicharron', alias: "Chicharron", idCategoria: 2, precio: 2, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Carnitas', alias: "Carnitas", idCategoria: 2, precio: 2, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Taco Chingón Cabeza', alias: "Cabeza", idCategoria: 2, precio: 2, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Pollo Asado', alias: "Pollo Asado", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Carne Asada', alias: "Carne Asada", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Carne Guisada', alias: "Carne Guisada", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Al Pastor', alias: "Al Pastor", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla', 'Piña']},
+  {name:'Taco Chingón Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 2, precio: 1.5, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Chicharron', alias: "Chicharron", idCategoria: 2, precio: 2, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Carnitas', alias: "Carnitas", idCategoria: 2, precio: 2, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Taco Chingón Cabeza', alias: "Cabeza", idCategoria: 2, precio: 2, eliminables:['Frejol','Queso','Ensalada','Arroz','Aguacate','Cebolla']},
   
-  {nombre:'Burrito Pollo Asado', alias: "Pollo Asado", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
-  {nombre:'Burrito Carne Asada', alias: "Carne Asada", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
-  {nombre:'Burrito Carne Guisada', alias: "Carne Guisada", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
-  {nombre:'Burrito Al Pastor', alias: "Al Pastor", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña'], agregables:['Dividido']},
-  {nombre:'Burrito Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
-  {nombre:'Burrito Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
-  {nombre:'Burrito Chicharron', alias: "Chicharron", idCategoria: 3, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña'], agregables:['Dividido']},
-  {nombre:'Burrito Carnitas', alias: "Carnitas", idCategoria: 3, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
-  {nombre:'Burrito Cabeza', alias: "Cabeza", idCategoria: 3, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Pollo Asado', alias: "Pollo Asado", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Carne Asada', alias: "Carne Asada", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Carne Guisada', alias: "Carne Guisada", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Al Pastor', alias: "Al Pastor", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña'], agregables:['Dividido']},
+  {name:'Burrito Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 3, precio: 2.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Chicharron', alias: "Chicharron", idCategoria: 3, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña'], agregables:['Dividido']},
+  {name:'Burrito Carnitas', alias: "Carnitas", idCategoria: 3, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
+  {name:'Burrito Cabeza', alias: "Cabeza", idCategoria: 3, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla'], agregables:['Dividido']},
   
-  {nombre:'Quesadilla Pollo Asado', alias: "Carne Asada", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Quesadilla Carne Asada', alias: "Carne Asada", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Quesadilla Carne Guisada', alias: "Carne Asada", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Quesadilla Al Pastor', alias: "Al Pastor", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña']},
-  {nombre:'Quesadilla Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Quesadilla Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Quesadilla Chicharron', alias: "Chicharron", idCategoria: 4, precio: 3.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña']},
-  {nombre:'Quesadilla Carnitas', alias: "Carnitas", idCategoria: 4, precio: 3.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
-  {nombre:'Quesadilla Cabeza', alias: "Cabeza", idCategoria: 4, precio: 3.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Pollo Asado', alias: "Carne Asada", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Carne Asada', alias: "Carne Asada", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Carne Guisada', alias: "Carne Asada", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Al Pastor', alias: "Al Pastor", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña']},
+  {name:'Quesadilla Cerdo Asado Picante', alias: "Cerdo Asado Picante", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Barbacoa de Res Picante', alias: "Barbacoa de Res Picante", idCategoria: 4, precio: 3, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Chicharron', alias: "Chicharron", idCategoria: 4, precio: 3.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla','Piña']},
+  {name:'Quesadilla Carnitas', alias: "Carnitas", idCategoria: 4, precio: 3.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
+  {name:'Quesadilla Cabeza', alias: "Cabeza", idCategoria: 4, precio: 3.5, eliminables:['Frejol','Ensalada','Arroz','Aguacate','Cebolla']},
 
-  {nombre:'Tortillas artesanales', idCategoria: 5, precio: 1},
-  {nombre: "Cola", idCategoria: 6, precio: 0.5},
-  {nombre: "Horchata", idCategoria: 6, precio: 0.5},
-  {nombre: "Cerveza", idCategoria: 6, precio: 2}
+  {name:'Tortillas artesanales', idCategoria: 5, precio: 1},
+  {name: "Cola", idCategoria: 6, precio: 0.5},
+  {name: "Horchata", idCategoria: 6, precio: 0.5},
+  {name: "Cerveza", idCategoria: 6, precio: 2}
   ];
 
 class PoS extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mesaSeleccionada: mesas[0].nombre,
-      clienteSeleccionado: clientes[0].nombre,
-      categoriaSeleccionada: categorias[0].nombre,
+      mesaSeleccionada: mesas[0].name,
+      clienteSeleccionado: clientes[0].name,
+      categoriaSeleccionada: categorias[0].name,
       productosActivos: productos.filter(producto => producto.idCategoria===1), // Se inicializa con los tacos
       //productoVisible: null
       focusedProduct: null,
@@ -117,7 +117,7 @@ class PoS extends Component {
     let productosActivos = productos.filter(producto => producto.idCategoria===categoria._id);
     
     this.setState({
-      categoriaSeleccionada: categoria.nombre,
+      categoriaSeleccionada: categoria.name,
       focusedProduct: null,
       productosActivos
     });
@@ -133,20 +133,27 @@ class PoS extends Component {
   }
 
   setFocusedProduct = (event) => {
-    if (this.state.focusedProduct && this.state.focusedProduct===event.target.textContent){
-      console.log('1');
+    let producto = JSON.parse(event.target.value);
+    if (this.state.focusedProduct && this.state.focusedProduct.name===producto.name){
       this.setState({focusedProduct:null});
     }else{
-      console.log('2');
       this.setState({
-        focusedProduct:event.target.textContent,
+        focusedProduct: producto,
         selectorCantidad: 1
       });
     }
   }
 
-  getProductColor = nombreProducto => {
-    if (this.state.focusedProduct && this.state.focusedProduct===nombreProducto){
+  isFocusedProduct = (producto) => {
+    if (this.state.focusedProduct && this.state.focusedProduct.name===producto.name){
+      return true;
+    }else{
+      return false
+    }
+  }
+
+  getProductColor = producto => {
+    if (this.state.focusedProduct && this.state.focusedProduct.name===producto.name){
       return "btn btn-danger w-100"
     }else{
       return "btn btn-secondary w-100"
@@ -177,6 +184,22 @@ class PoS extends Component {
     }
   }
 
+   onClickAceptarProducto = event =>{
+    let product = this.state.focusedProduct;
+    let cantidad = this.state.selectorCantidad;
+    let addCanasta = [];
+    for (let i=0; i< cantidad; i++){
+      let newProduct = {id: + new Date(), name: product.name};
+      addCanasta.push(newProduct);
+    }
+    this.setState({
+      canasta: [...this.state.canasta, ...addCanasta],
+      focusedProduct: null
+    });
+    console.log(this.state.canasta)
+
+  }
+
 	render() {
 		return (
 			<>
@@ -190,7 +213,7 @@ class PoS extends Component {
                 </button>
                 <ul className="dropdown-menu pos-dropdown-list">
                   {mesas.map(mesa => (
-                    <li key={mesa.nombre}><a className="dropdown-item pos-dropdown-item" href="#" onClick={this.onSelectMesa}>{mesa.nombre}</a></li>
+                    <li key={mesa.name}><a className="dropdown-item pos-dropdown-item" href="#" onClick={this.onSelectMesa}>{mesa.name}</a></li>
                   ))
                   }
                 </ul>
@@ -203,7 +226,7 @@ class PoS extends Component {
                 </button>
                 <ul className="dropdown-menu pos-dropdown-list">
                   {clientes.map(cliente => (
-                    <li key={cliente.nombre}><a className="dropdown-item pos-dropdown-item" href="#" onClick={this.onSelectCliente}>{cliente.nombre}</a></li>
+                    <li key={cliente.name}><a className="dropdown-item pos-dropdown-item" href="#" onClick={this.onSelectCliente}>{cliente.name}</a></li>
                   ))
                   }
                 </ul>
@@ -219,8 +242,8 @@ class PoS extends Component {
               <div className="btn-group-vertical">
                 {categorias.map(categoria => (
                   <>
-                    <input type="radio" className="btn-check" name="btnradio" id={categoria._id} autoComplete="off" onChange={this.onSelectCategoria} checked={this.checkRadioButton(categoria.nombre)}/>
-                    <label className="btn btn-outline-success pos-radio-button" htmlFor={categoria._id}>{categoria.nombre}</label>
+                    <input type="radio" className="btn-check" name="btnradio" id={categoria._id} autoComplete="off" onChange={this.onSelectCategoria} checked={this.checkRadioButton(categoria.name)}/>
+                    <label className="btn btn-outline-success pos-radio-button" htmlFor={categoria._id}>{categoria.name}</label>
                   </>
                 ))
                 }
@@ -232,16 +255,16 @@ class PoS extends Component {
             <div className="col-9">
               <div className="row">
                 {this.state.productosActivos.map(producto => (
-                  //<div className={this.getProductVisibility(producto.alias?producto.alias:producto.nombre)} key={producto.nombre}>
-                  <div className="col-6 col-md-4 py-1" key={producto.nombre}>
+                  //<div className={this.getProductVisibility(producto.alias?producto.alias:producto.name)} key={producto.name}>
+                  <div className="col-6 col-md-4 py-1" key={producto.name}>
                     <div className="row">
 
                       <div className="col-12">
-                        <button className={this.getProductColor(producto.alias?producto.alias:producto.nombre)} onClick={this.setFocusedProduct}>                        
-                          {producto.alias?producto.alias:producto.nombre}
+                        <button className={this.getProductColor(producto)} value={JSON.stringify(producto)} onClick={this.setFocusedProduct}>                        
+                          {producto.alias?producto.alias:producto.name}
                         </button>
                       </div>
-                      {this.state.focusedProduct && this.state.focusedProduct===(producto.alias?producto.alias:producto.nombre)?
+                      {this.isFocusedProduct(producto)?
                       <>
                         <div className="col-3 selector-cant-l">
                           <button className="btn btn-light w-100 selector-border-l" value="-" onClick={this.setCantidad}>
@@ -257,14 +280,12 @@ class PoS extends Component {
                           </button>
                         </div>
                         <div className="col-12">
-                          <button className="btn btn-success btn-sm w-100">
+                          <button className="btn btn-success btn-sm w-100" onClick={this.onClickAceptarProducto}>
                             ACEPTAR
                           </button>
                         </div>
                       </>:null
                       }
-
-
                     </div>                  
                   </div>                  
                 ))}
