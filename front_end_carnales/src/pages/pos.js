e87e3e02acf080713308cfaa26228ac251107894
Pablo Villota <pvillota@cuenca.gob.ec>
2022-06-30 23:19:09 -0500

    simplifica función de resumen e implementa eliminar producto

--- front_end_carnales/src/pages/pos.js
+++ front_end_carnales/src/pages/pos.js
@@ -94,7 +94,9 @@ class PoS extends Component {
       focusedProduct: null,
       selectorCantidad: 1,
       bufferProductos:[],
-      canastas:[]
+      canastas:[],
+      cuentasClientes: [],
+      cuentaTotal: (0).toFixed(2),
       //productoVisible: "Carne Asada"
     };
   }
@@ -186,92 +188,98 @@ class PoS extends Component {
     }
   }
 
-   onClickAceptarProducto = event =>{
+  onClickAceptarProducto = event =>{
     let cliente = this.state.clienteSeleccionado;
     let product = this.state.focusedProduct;
     let cantidad = this.state.selectorCantidad;
     let addBuffer = [];
     for (let i=0; i< cantidad; i++){
-      // se suma i al id para evitar ids repetidos en la misma iteración
-      let newProduct = {id: + new Date() + i, name: product.name, cliente: cliente};
+      // se suma i al instanceID para evitar ids repetidos en la misma iteración
+      let newProduct = {instanceID: + new Date() + i, name: product.name, precio:product.precio, cliente: cliente};
       addBuffer.push(newProduct);
     }
+    let {cuentaTotal, cuentasClientes} = this.getCuentas([...this.state.bufferProductos, ...addBuffer]);
     this.setState({
+      cuentaTotal: cuentaTotal.toFixed(2),
+      cuentasClientes,
       bufferProductos: [...this.state.bufferProductos, ...addBuffer],
       focusedProduct: null
     });
   }
 
   onClickContinuar = event =>{
+    //in:  producto:{instanceID, name, precio, cliente, observaciones}[];
     let bufferCanastas = [];
-    //FORMATO: {cliente, productos}
-    let bufferProductos = JSON.parse(JSON.stringify(this.state.bufferProductos)); // para hacer una copia profunda
+    let productosNoProcesados = this.state.bufferProductos;
     // Separa los productos en el buffer en diferentes canastas según los clientes
-    for (let i=0; i< bufferProductos.length; i++){
-      if (i===0){
-        let newCanasta = {cliente: bufferProductos[i].cliente, productos:[]}
-        delete bufferProductos[i].cliente;
-        newCanasta.productos.push(bufferProductos[i]);
-        bufferCanastas.push(newCanasta);
-        continue;
-      }
-      // Compara si el cliente del producto coincide con una canasta existente, y si no crea una nueva
-      for(let j=0; j< bufferCanastas.length; j++){
-        if (bufferProductos[i].cliente===bufferCanastas[j].cliente){
-          delete bufferProductos[i].cliente;
-          bufferCanastas[j].productos.push(bufferProductos[i]);
-          break;
-        }else if (j === bufferCanastas.length-1){
-          let newCanasta = {cliente: bufferProductos[i].cliente, productos:[]}          
-          delete bufferProductos[i].cliente;
-          newCanasta.productos.push(bufferProductos[i]);
-          bufferCanastas.push(newCanasta);
-        }
-      }
+    //FORMATO out: {cliente, productos[]}
+    while (productosNoProcesados.length>0){
+      let nombreClienteProducto = productosNoProcesados[0].cliente;
+      let selectedProducts = productosNoProcesados.filter(producto => producto.cliente === nombreClienteProducto);
+      productosNoProcesados = productosNoProcesados.filter(producto => producto.cliente !== nombreClienteProducto);
+      // Procesa los elementos seleccionados      
+      let productos = [];
+      selectedProducts.forEach(producto => productos.push({name:producto.name})) // añadir observaciones aqui
+      // Se resumen los productos de cada cliente:
+      // Formato: {name, cantidad, observacion}]
+      let resumenProductos = this.resumirProductos(productos);
+      let newCanasta = {cliente: nombreClienteProducto, productos:resumenProductos};
+      bufferCanastas.push(newCanasta);
     }
-    // Se establecerá el formato de las canastas como:
-    // {cliente, productos:[{nombre, cantidad, observacion}]}
-    // previamente productos llega como productos:[{id, name, observacion}]
-    let canastas = [];
-    for (let i=0; i< bufferCanastas.length; i++){
-      let productosCanasta = [];
-      let productosBuffer = bufferCanastas[i].productos;
-      for (let j=0; j< productosBuffer.length; j++){
-        let productCompBuffer = {...productosBuffer[j]};
-        delete productCompBuffer.id;
-        if (j>0){
-          for(let k=0; k< productosCanasta.length; k++){
-            let productCompCanasta = {...productosCanasta[k]};
-						delete productCompCanasta.cantidad;
-						if (_.isEqual(productCompCanasta, productCompBuffer)){
-							// Si está repetido le suma 1 a la cantidad
-							productosCanasta[k].cantidad++;
-							break;
-						}else if(k==productosCanasta.length-1){
-							// Si es un producto nuevo crea ese producto
-							productCompBuffer.cantidad=1;
-							productosCanasta.push({...productCompBuffer});
-              break; // Es necesario porque se modifica el tamaño de productosCanasta
-						}
-          }
-        }else{
-          productCompBuffer.cantidad=1;
-					productosCanasta.push({...productCompBuffer});
-        }
-      }
-      bufferCanastas[i].productos=productosCanasta;
-      canastas.push(bufferCanastas[i]);
-    }
-    this.setState({canastas})
 
-    fetch('http://localhost:4000/api/print', {
+    console.log(bufferCanastas)
+    this.setState({bufferCanastas})
+
+    /* fetch('http://localhost:4000/api/print', {
       method: 'POST', 
       body: JSON.stringify(canastas[0]),
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       }
-      }).then(console.log('s'));
+      }).then(console.log('Enviado')); */
+  }
+
+  resumirProductos = arrayProductos => {
+    //in-> [{instanceID, name, precio, cliente, observaciones}]
+    //out-> [{name, cantidad, observaciones}]
+    let newArrayProductos = [];
+    while (arrayProductos.length>0){
+      let productoAProcesar = {...arrayProductos[0]};
+      let selectedProducts = arrayProductos.filter(producto => _.isEqual(producto, productoAProcesar)); // Existe forma de contar sin filtrar
+      arrayProductos = arrayProductos.filter(producto => !_.isEqual(producto, productoAProcesar));
+      //let newProducto = {name: productoAProcesar.name, observacion:productoAProcesar.observacion, cantidad: selectedProducts.length}
+      let newProducto = {name: productoAProcesar.name, cantidad: selectedProducts.length};
+      newArrayProductos.push(newProducto);
+    }
+    return newArrayProductos;
+  }
+
+  getCuentas = productosNoProcesados => {
+    //in: {instanceID, name, precio, cliente}
+    let cuentasClientes =[];
+    while (productosNoProcesados.length>0){
+      let nombreProductoActual = productosNoProcesados[0].name;
+      let selectedProducts = productosNoProcesados.filter(producto => producto.name===nombreProductoActual);
+      productosNoProcesados = productosNoProcesados.filter(producto => producto.name!==nombreProductoActual); // Actualiza productos sin procesar
+      //Calcula la cuenta de todos los productos
+      let cuentaTotalCliente = 0;
+      selectedProducts.forEach(producto => { cuentaTotalCliente = cuentaTotalCliente + producto.precio });
+      let cuentaTemp = { total: cuentaTotalCliente, cliente:selectedProducts[0].cliente }
+      cuentasClientes.push(cuentaTemp);      
+    }
+    let cuentaTotal = 0;
+    cuentasClientes.forEach(cuenta => { cuentaTotal = cuentaTotal + cuenta.total });
+    //this.setState({cuentaTotal, cuentasClientes});
+    return {cuentaTotal, cuentasClientes}
+  }
+
+  onClickEliminar = event => {
+    let instanceID = event.target.id;
+    console.log(instanceID);
+    let productos = this.state.bufferProductos;
+    let newBufferProductos = productos.filter(producto => producto.instanceID!==Number(instanceID));
+    this.setState({bufferProductos: newBufferProductos})
   }
 
 	render() {
@@ -373,7 +381,7 @@ class PoS extends Component {
                     {this.state.bufferProductos.map(product=>(
                       <tr>
                         <td className="products-cell">
-                          <button className="btn btn-danger products-control">
+                          <button className="btn btn-danger products-control" id={product.instanceID} onClick={this.onClickEliminar}>
                           X
                           </button>
                         </td>
@@ -388,8 +396,13 @@ class PoS extends Component {
           </div>
         </div>
 
-        <div className="container-md">
+        <div className="container-md pt-3">
           <div className="row">
+            <div className="col-6 checkout-container text-center">
+              <h2>
+                Total: ${this.state.cuentaTotal}
+              </h2>
+            </div>
             <div className="col-6 checkout-container">
               <button className="btn btn-success btn-lg w-75" onClick={this.onClickContinuar}>
               CONTINUAR
